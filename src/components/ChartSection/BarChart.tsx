e2ec999d122ac66ceb12c4b9dd8f845f7d517133
Jenith-Panchal-Simform <jenith.panchal@simformsolutions.com>
2026-06-16 16:39:46 +0530

    fix:fix step and scroll of chart

--- src/components/ChartSection/BarChart.tsx
+++ src/components/ChartSection/BarChart.tsx
@@ -4,20 +4,16 @@ export const BarChart = () => {
   const { chartData } = useChartData();
   const { axis, data } = chartData;
 
-  const maxVal = Math.max(...data.map((item) => item.value));
-  const numberOfLines = data.length;
+  const maxVal = data.length !== 0 ? Math.max(...data.map((item) => item.value)) : 0;
 
-  const step = Math.ceil(maxVal / numberOfLines);
+  const step = maxVal / 10;
 
-  const arr = Array.from({ length: data.length }, (_, i) => {
-    return i * step;
+  const arr = Array.from({ length: 11 }, (_, i) => {
+    return (i * step).toFixed(2);
   });
-  if (arr.length > 0) {
-    arr.push(maxVal);
-  }
 
   return (
-    <main className="relative flex-9 overflow-hidden pl-10">
+    <main className="custom-scrollbar relative flex-9 overflow-x-auto pl-10">
       <h1 className="text-xl font-bold">Bar Chart</h1>
       <div
         className="absolute top-1/2 left-2 -translate-y-1/2 rotate-180 font-bold whitespace-nowrap"
@@ -29,11 +25,11 @@ export const BarChart = () => {
         {axis.yAxisLabel}
       </div>
 
-      <div className="absolute bottom-1 left-1/2 -translate-x-1/2 font-bold whitespace-nowrap">
+      <div className="absolute bottom-0 left-1/2 -translate-x-1/2 font-bold whitespace-nowrap">
         {axis.xAxisLabel}
       </div>
 
-      <div className="justify-betwen mx-5 my-2 flex h-[calc(100vh-80px)] items-end gap-2 overflow-x-auto border-b-2 border-l-2">
+      <div className="justify-betwen mx-5 my-2 flex h-[calc(100vh-80px)] items-end gap-2 border-b-2 border-l-2">
         <div className="item flex h-[calc(100vh-100px)] flex-col-reverse justify-between">
           {arr.map((item) => (
             <span>{item}</span>
@@ -45,13 +41,13 @@ export const BarChart = () => {
             className="flex h-[calc(100vh-100px)] flex-col-reverse items-center"
           >
             <div
-              className="w-25 rounded-t bg-blue-500 transition-all duration-300 ease-out hover:bg-blue-600"
+              className="relative w-25 rounded-t bg-blue-500 transition-all duration-300 ease-out hover:bg-blue-600"
               style={{
                 height: `${(item.value / maxVal) * 100}%`,
               }}
               title={`x:${item.title} y:${item.value}`}
             />
-            <span className="absolute bottom-6 flex w-25 justify-center text-sm">
+            <span className="absolute bottom-3 flex w-25 justify-center text-sm">
               {item.title.length > 5 ? item.title.slice(0, 3) : item.title}
             </span>
           </div>
